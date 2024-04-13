fec448cc34939a164cf697d541178ea59881c462
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2024-04-13 20:41:53 -0300

    create routes and pagination

---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+import React from "react"
+
+export default function SignInPage() {
+  return (
+    <PageContainer>
+      <Title>Login</Title>
+    </PageContainer>
+  )
+}
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'Roboto';
+    font-size: 24px;
+    text-align: center;
+    color: #293845;
+    margin-top: 30px;
+    padding-top: 70px;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 16px;
+`;
