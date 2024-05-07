17fb308d9e533386eb3b338761a0862cd536e198
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2024-05-07 09:35:22 -0300

    feat: layout and main logic footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,40 @@ import styled from "styled-components"
 
 export default function FooterBar() {
     return (
-        <Footer></Footer>
+        <Footer>
+            <p>© Processo de Trainee <a href="https://unect.com.br/" target="_blank">Unect Jr.</a> </p>
+            <p>Feito com <span class="material-icons">favorite</span> por <a href="https://github.com/Gabriel-16n5" target="_blank">Gabriel Granjeia</a></p>
+        </Footer>
     )
 }
 
 const Footer = styled.div`
-    background-color: #226ED8;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color:#114FA7;
     width: 100%;
     height: 52px;
     position: fixed;
     bottom: 0;
     left: 0;
     z-index: 1000;
-    box-shadow: 0px 0px 15px 2px #000;
+    p{
+        display: flex;
+        font-family: Poppins;
+        font-size: 20px;
+        text-align: left;
+        color: #FFFFFF;
+        span{
+            margin-left: 5px;
+            margin-right: 5px;
+            color: #FFAFAF;
+        }
+        a{
+            color: inherit;
+            text-decoration: underline;
+            font-weight: 700;
+            margin-left: 5px;
+        }
+    }
 `
