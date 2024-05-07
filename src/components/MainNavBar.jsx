86b6620b96ffc64f1e7383990988509ebe1e286d
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2024-05-07 09:08:50 -0300

    feat: main layout and logic home navbar done

--- src/components/MainNavBar.jsx
+++ src/components/MainNavBar.jsx
@@ -1,18 +1,87 @@
 import styled from "styled-components"
+import logoBranco from "../assets/LogoBranca.png"
+import Switch from "react-switch";
+import { useState } from "react";
 
-export default function MainNavBar() {
+
+
+export default function MainNavBar(props) {
+    const {setDarkMode} = props
+    const [darkModeVerify, setDarkModeVerify] = useState(false);
+
+    const switchHandlerDarkMode = (checked) => {
+        setDarkModeVerify(checked);
+        if(darkModeVerify === false) setDarkMode(true);
+        if (darkModeVerify === true) setDarkMode(false);
+    };
+
     return (
-        <MainNav></MainNav>
+        <MainNav>
+            <img src={logoBranco} alt="logo branca" />
+            <Title>uTask 3.0</Title>
+            <ButtonContainer>
+            <Button>
+                <Switch
+                checked={darkModeVerify}
+                onChange={switchHandlerDarkMode}
+                offColor="#FFC93F"
+                onColor="#222222"
+                />
+                {/* <p>Dark Mode</p> */}
+            </Button>
+        </ButtonContainer>
+        </MainNav>
     )
 }
 
 const MainNav = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
     background-color: #226ED8;
     width: 100%;
-    height: 52px;
+    height: 80px;
     position: fixed;
     top: 0;
     left: 0;
     z-index: 1000;
-    box-shadow: 0px 0px 15px 2px #000;
+    box-shadow: 0px 0px 10px 2px #0000004D;
+    img {
+        width: 23.28px;
+        height: 40px;
+        top: 20px;
+        left: 160px;
+        gap: 0px;
+        opacity: 0px;
+    }
+`
+
+const Title = styled.h1`
+    color: #FAFAFA;
+    font-family: Poppins, sans-serif;
+    font-size: 42px;
+    font-weight: 700;
+    line-height: 63px;
+    text-align: center;
+`
+
+const Button = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: start;
+    /* p{
+        font-size:.7vw;
+        margin-top: 4px;
+        margin-left: 10px;
+        font-family: 'Open Sans';
+        @media(max-width: 1540px) {
+        font-size: 5vw;
+    }
+    } */
+`
+const ButtonContainer = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
 `
